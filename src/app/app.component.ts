bad03e8e9528bcf151fa1fff1fa80857031e8008
ahmet <ahmet.uyanik.dev@gmail.com>
2023-12-05 23:49:48 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: '<h1>Hello world!</h1> <br/> <h2>{{title}}</h2>',
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'homes';
+  names : string [] = [];
+
+  constructor() {
+  }
+
+  ngOnInit(){
+    console.log('on init');
+    this.names = ['Ahmet','Sedat','Mehmet'];
+  }
+
+  addNewPerson(value:string){
+    console.log('Adding new person: '+value);
+    this.names.push(value);
+    value="";
+  }
+
+  isNullOrEmptyOrUndefined(value : string){
+    return !value;
+  }
+
 }
